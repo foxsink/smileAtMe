57de134c9c1f65043ab914a7567523635aeaef13
Eugene <youkarpov@gmail.com>
2023-01-29 15:35:03 +0700

    added default layout with navigation list inside + added fonts

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="navigation">
+      <div class="container">
+        <div class="navigation-list">
+          <ul>
+            <li><a href="#" class="active">На главную</a></li>
+            <li><a href="#">Создать свою карточку</a></li>
+            <li><a href="#">Карточки других пользователей</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped lang="scss">
+  @import "Navigation-list";
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
+  <NuxtLayout>
+    <default></default>
+  </NuxtLayout>
   <Card />
 </template>
 
 <script>
 import Card from "~/src/Card.vue";
+import Default from "~/layouts/default.vue";
 
 export default {
   name: "index",
-  components: {Card}
+  components: {Default, Card}
 }
 </script>
 
