f47560da0715696ec66bcf22c2926c60d0201b16
Ярослав Карпов <iar.karpov@gmail.com>
2023-02-01 04:01:07 +0700

    fix styles

--- src/Card.vue
+++ src/Card.vue
@@ -1,21 +1,19 @@
 <template>
   <div v-for="post in posts" class="card">
-    <div class="card-image">
+    <div class="card-image card__item">
       <img
           class="card-image__img"
           :src="`/_nuxt/${post.image_path}`"
           alt="Я и без того знаю, какой ты Петросян"
       >
     </div>
-    <div class="card-caption">
-      Обстановка в России
-    </div>
-    <div class="card-text">
-      <p class="card-text__p">
-        {{ post.article }}
-      </p>
-    </div>
-    <div class="card-audio">
+    <span class="card__caption card__item">
+      {{ post.image_title }}
+    </span>
+    <span class="card__text card__item">
+      {{ post.article }}
+    </span>
+    <div class="card-audio card__item">
       <audio
           :src="`/_nuxt/${post.audio_path}`"
           class="card-audio__associated-audio"
