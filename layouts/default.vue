53930c1d1bce37bea6f981caf82cd63cab82f7eb
Eugene <youkarpov@gmail.com>
2023-02-01 03:30:12 +0700

    card complete without nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,35 @@
 <template>
-  <header>
-    <nav class="navigation">
-      <div class="navigation-background"></div>
-      <ul class="navigation__list navigation-list">
-        <nuxt-link href="/" class="navigation-list-link">
-          <li class="navigation-list-link__item">
-            На главную
-          </li>
-        </nuxt-link>
-        <nuxt-link href="/card/create" class="navigation-list-link">
-          <li class="navigation-list-link__item">
-            Создать свою карточку
-          </li>
-        </nuxt-link>
-        <nuxt-link href="/gallery" class="navigation-list-link">
-          <li class="navigation-list-link__item">
-              Карточки других пользователей
-          </li>
-        </nuxt-link>
-      </ul>
-    </nav>
-  </header>
-  <main class="main-container">
-    <slot />
-  </main>
-  <footer>
-Ты лох
-  </footer>
+  <div class="body-container">
+    <header>
+      <nav class="navigation">
+        <div class="navigation-background"></div>
+        <div class="navigation-border"></div>
+        <ul class="navigation__list navigation-list">
+          <nuxt-link href="/" class="navigation-list-link">
+            <li class="navigation-list-link__item">
+              На главную
+            </li>
+          </nuxt-link>
+          <nuxt-link href="/card/create" class="navigation-list-link">
+            <li class="navigation-list-link__item">
+              Создать свою карточку
+            </li>
+          </nuxt-link>
+          <nuxt-link href="/gallery" class="navigation-list-link">
+            <li class="navigation-list-link__item">
+                Карточки других пользователей
+            </li>
+          </nuxt-link>
+        </ul>
+      </nav>
+    </header>
+    <main class="main-container">
+      <slot />
+    </main>
+    <footer>
+  Ты лох
+    </footer>
+  </div>
 </template>
 
 <script>
--- src/Card.vue
+++ src/Card.vue
@@ -1,32 +1,30 @@
 <template>
-    <div v-for="post in posts" class="card">
-      <div class="card-image">
-        <figure class="card-image__figure">
-          <img
-              class="card-image__img"
-              :src="`/_nuxt/${post.image_path}`"
-              alt="Я и без того знаю, какой ты Петросян"
-          >
-          <figcaption class="card-figcaption">
-            Обстановка в России
-          </figcaption>
-        </figure>
-      </div>
-      <div class="card-text">
-        <p class="card-text__p">
-          {{ post.article }}
-        </p>
-      </div>
-      <div class="card-audio">
-        <audio
-            :src="`/_nuxt/${post.audio_path}`"
-            class="card-audio__associated-audio"
-            controls
-        >
-          Your browser does not support the audio element.
-        </audio>
-      </div>
+  <div v-for="post in posts" class="card">
+    <div class="card-image">
+      <img
+          class="card-image__img"
+          :src="`/_nuxt/${post.image_path}`"
+          alt="Я и без того знаю, какой ты Петросян"
+      >
     </div>
+    <div class="card-caption">
+      Обстановка в России
+    </div>
+    <div class="card-text">
+      <p class="card-text__p">
+        {{ post.article }}
+      </p>
+    </div>
+    <div class="card-audio">
+      <audio
+          :src="`/_nuxt/${post.audio_path}`"
+          class="card-audio__associated-audio"
+          controls
+      >
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  </div>
 </template>
 
 <script setup>
