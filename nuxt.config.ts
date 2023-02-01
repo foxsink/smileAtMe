147ca4d55496260514fd7d7c1a787a9910a658c6
Eugene <youkarpov@gmail.com>
2023-02-02 00:19:33 +0700

    fix fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: ['@/assets/globalStyles/global.scss', '@/assets/globalStyles/_variables.scss'],
+    css: ['@/assets/globalStyles/_variables.scss', '@/assets/globalStyles/global.scss'],
     vite: {
         server: {
             watch: {
